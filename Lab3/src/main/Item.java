30d62e46c6ce6abb4e66b10e16650f04b5254fff
Nikkkt <work.terpilovsky@gmail.com>
2024-11-03 23:39:05 +0200

    Refactor Item class and related logic to wrap all primitives and strings. Introduced SellIn and Quality wrapper classes to encapsulate their behaviors and improve code maintainability

--- Lab3/src/main/Item.java
+++ Lab3/src/main/Item.java
@@ -1,21 +1,30 @@
 package main;
 
 public class Item {
+    private final String name;
+    private final SellIn sellIn;
+    private final Quality quality;
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
+    public Item(String name, SellIn sellIn, Quality quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public SellIn getSellIn() {
+        return sellIn;
+    }
+
+    public Quality getQuality() {
+        return quality;
+    }
+
     @Override
     public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+        return name + ", " + sellIn.getValue() + ", " + quality.getValue();
     }
 }
